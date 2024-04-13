cc2bda9d8d7eedc6cc2703e28070b977054d62d3
khanabdu25 <khanabdu@umich.edu>
2024-04-13 14:11:10 -0400

    init
    has tailwind, has app.tsx only

---
+++ veah/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 bg-black justify-center items-center">
+      <Text className="text-white">
+        Open uppp.tsx to start aaaaaaaa on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
